a6be304727de649e3ccfb3dc2f862fe97559bafd
Xelaflash <xelaflash@gmail.com>
2018-09-21 15:16:35 -0400

    app finished

---
+++ src/components/gif.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Gif extends Component {
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
+  }
+
+  render() {
+    const src = `https://i.giphy.com/media/${this.props.id}/giphy.gif`;
+    return (
+      <img src={src} alt="gif" className="gif" onClick={this.handleClick} />
+    );
+  }
+}
+
+export default Gif;
