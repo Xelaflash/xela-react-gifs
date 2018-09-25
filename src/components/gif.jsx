f0bda38270fa838eccaf78a4f1c3cf3c6fb0716c
Xelaflash <xelaflash@gmail.com>
2018-09-25 10:05:58 -0400

    final

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
 
 class Gif extends Component {
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.id !== this.props.id;
+  shouldComponentUpdate(nextProps) {
+    return nextProps.id !== this.props.id; // dont call render if props.id did not change
   }
 
-handleClick = () => {
-  if (this.props.selectGif) {
-    this.props.selectGif(this.props.id);
+  handleClick = () => {
+    if (this.props.selectGif) {
+      this.props.selectGif(this.props.id);
+    }
   }
-}
 
-render() {
-// handling null
-// if (!this.props.id) {
-//   return null;
-// }
-const src = `https://i.giphy.com/media/${this.props.id}/giphy.gif`;
-  return (
-    <img src={src} alt="gif" className="gif" onClick={this.handleClick} />
-  );
+  render() {
+  // handling null
+  // if (!this.props.id) {
+  //   return null;
+  // }
+    const src = `https://i.giphy.com/media/${this.props.id}/giphy.gif`;
+    return (
+      <img src={src} alt="gif" className="gif" onClick={this.handleClick} />
+    );
+  }
 }
 
 export default Gif;
--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 
 
 class SearchBar extends Component {
+  shouldComponentUpdate() {
+    return false;
+  }
+
   handleUpdate = (event) => {
     this.props.searchFunction(event.target.value);
   }
